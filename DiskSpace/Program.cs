1bfa427a139c84a51dc2459a6d910e503faf208e
Todd Reifsteck <toddreif@microsoft.com>
2015-03-03 21:43:29 -0800

    Minor cleanup of code

--- DiskSpace/Program.cs
+++ DiskSpace/Program.cs
@@ -13,51 +13,59 @@ namespace DiskSpace
         static void Main(string[] args)
         {
             Stopwatch watch = Stopwatch.StartNew();
-            var dirs = new Dirs("COMPUTER", null);
+            var dirs = new Dirs("COMPUTER");
             string[] drives = Directory.GetLogicalDrives();
             foreach (var drive in drives)
             {
                 Console.WriteLine(drive);
-                var subDir = AddSubDirectories(drive, drive, dirs);
-                if (subDir != null && subDir.totalSize > 0)
+                var subDir = AddSubDirectories(drive, drive);
+                if (subDir != null && subDir.size > 0)
                 {
                     dirs.directories.Add(subDir);
-                    dirs.totalSize = dirs.totalSize + subDir.totalSize;
+                    dirs.size = dirs.size + subDir.size;
                 }
             }
             Console.WriteLine(watch.Elapsed);
 
             var json = Jil.JSON.Serialize(dirs);
             File.AppendAllText("C:\\foo.json", json);
-            Console.ReadLine();
         }
 
-        public static Dirs AddSubDirectories(string path, string name, Dirs dirs)
+        public static Dirs AddSubDirectories(string path, string name)
         {
-            Dirs currentDir = new Dirs(name, path);
+            Dirs currentDir = null;
             try
             {
-                FileInfo file;
                 foreach (var direct in new DirectoryInfo(path).EnumerateFileSystemInfos("*"))
                 {
-                    //direct.Attributes.HasFlag(FileAttributes.Directory)
-                    file = direct as FileInfo;
-                    if (file == null)
-                    {
-                        // Track directory for future addition
-                        var subDir = AddSubDirectories(direct.FullName, direct.Name, currentDir);
-                        if (subDir != null && subDir.totalSize > 0)
+                    if (!direct.Attributes.HasFlag(FileAttributes.ReparsePoint))
+                        if (direct.Attributes.HasFlag(FileAttributes.Directory))
                         {
-                            currentDir.directories.Add(subDir);
-                            currentDir.totalSize = currentDir.totalSize + subDir.totalSize;
+                            // Track directory for future addition
+                            var subDir = AddSubDirectories(direct.FullName, direct.Name);
+                            if (subDir != null && subDir.size > 0)
+                            {
+                                if (currentDir == null)
+                                {
+                                    currentDir = new Dirs(name);
+                                }
+
+                                currentDir.directories.Add(subDir);
+                                currentDir.size = currentDir.size + subDir.size;
+                            }
                         }
-                    }
-                    else
-                    {
-                        // File
-                        currentDir.files.Add(new FileSize(file.Name, file.Length));
-                        currentDir.totalSize = currentDir.totalSize + file.Length;
-                    }
+                        else
+                        {
+                            if (currentDir == null)
+                            {
+                                currentDir = new Dirs(name);
+                            }
+
+                            // File
+                            FileInfo file = (FileInfo)direct;
+                            currentDir.files.Add(new FileSize(file.Name, file.Length));
+                            currentDir.size = currentDir.size + file.Length;
+                        }
                 }
             }
             catch (UnauthorizedAccessException)
@@ -70,29 +78,20 @@ namespace DiskSpace
                 Console.WriteLine(e);
             }
 
-            if (currentDir.totalSize > 0)
-            {
-                return currentDir;
-            }
-            else
-            {
-                return null;
-            }
+            return currentDir;
         }
     }
 
     public class Dirs
     {
         public string name;
-        public string path;
-        public long totalSize;
+        public long size;
         public IList<Dirs> directories = new List<Dirs>();
-        public IList<FileSize> files = new List<FileSize>();   
-     
-        public Dirs(string name, string path)
+        public IList<FileSize> files = new List<FileSize>();
+
+        public Dirs(string name)
         {
             this.name = name;
-            this.path = path;
         }
     }
 
@@ -100,7 +99,7 @@ namespace DiskSpace
     {
         public string name;
         public long size;
-        
+
         public FileSize(string name, long size)
         {
             this.name = name;
